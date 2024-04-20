0903f38e4bc30da23029e4c1ffc596ca2526aedc
Wikidips <wikidips18@gmail.com>
2024-04-20 02:47:32 +0100

    hj

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,7 @@
+# installinf flask uing ppept
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+}
